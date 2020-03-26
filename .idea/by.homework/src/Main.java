fef60c471a90c82a1a929e5d0ed65522c8c8e3b7
Zablotski31 <58586811+Zablotski31@users.noreply.github.com>
2020-03-26 12:29:57 +0300

    1 commit

--- .idea/by.homework/src/Main.java
+++ .idea/by.homework/src/Main.java
@@ -2,7 +2,7 @@ package by.homework;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!11111");
         System.out.println("bye");
     }
 
